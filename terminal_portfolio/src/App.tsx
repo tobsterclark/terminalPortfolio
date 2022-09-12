7333dc7aa9eb0d0988c10713b033b0ebe3d2e036
tobsterclark <tobsterclark@gmail.com>
2022-09-12 19:31:58 +1000

    starting cra

---
+++ terminal_portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Input from "./components/Input";
+
+const App = () => {
+	return (
+		<div className="w-screen h-screen">
+			<div className="absolute z-10 bottom-0">
+				<Input />
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ terminal_portfolio/src/components/Input.tsx
@@ -0,0 +1,5 @@
+const Input = () => {
+	return <div className="h-fit w-screen">Main: test</div>;
+};
+
+export default Input;
